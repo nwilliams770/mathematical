a0d5c07932b555e4130985770388eb54119f8f48
Nicholas Williams <nichwilliams@tesla.com>
2024-05-30 16:46:33 -0700

    move to SDL and bresenham initial

--- src/renderer/renderer.cpp
+++ src/renderer/renderer.cpp
@@ -59,6 +59,21 @@ int Renderer::init() {
   return 0; // Zero indicates success
 };
 
+void Renderer::clear()
+{
+  setColor(RendererConstants::CLEAR_COLOR);
+  SDL_RenderClear(renderer);
+};
+
+void Renderer::present()
+{
+  SDL_RenderPresent(renderer);
+};
+
+void Renderer::setColor(const SDL_Color& color) {
+  SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
+}
+
 void Renderer::handleEvents(bool& running)
 {
   SDL_Event event;
@@ -72,29 +87,41 @@ void Renderer::handleEvents(bool& running)
   }
 }
 
-void Renderer::clear()
+void Renderer::renderPoint(int x, int y)
 {
-  // TODO use const color
-  setColor({0, 0, 0, SDL_ALPHA_OPAQUE}); // Clear with black color
-  SDL_RenderClear(renderer);
-};
+  setColor(RendererConstants::DEBUG_COLOR);
+  int scaledX = x * RendererConstants::SCALE;
+  int scaledY = y * RendererConstants::SCALE;
 
-void Renderer::present()
-{
-  SDL_RenderPresent(renderer);
-};
+  SDL_Rect rect;
+  rect.x = scaledX;
+  rect.y = scaledY;
+  rect.w = RendererConstants::SCALE;
+  rect.h = RendererConstants::SCALE;
+  SDL_RenderFillRect(renderer, &rect);
 
-void Renderer::setColor(const SDL_Color& color) {
-  SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
 }
-
-void Renderer::renderPoint(int x, int y) { }
 
 void Renderer::renderLine(int x1, int y1, int x2, int y2)
 {
-  int dx = x1 - x2;
-  int dy = y1 - y2;
-
+  // detect horizontal, vertical, diagnoal
+  drawLineBresenham(x1, y1, x2, y2);
+  if (x1 == x2)
+  {
+    drawVerticalLine(x1, y1, y2);
+  }
+  else if (y1 == y2)
+  {
+    drawHorizontalLine(x1, x2, y1);
+  }
+  else if (abs(x2 - x1) == abs(y2 - y1))
+  {
+    drawDiagonalLine(x1, y1, x2, y2);
+  }
+  else
+  {
+    drawLineBresenham(x1, y1, x2, y2);
+  }
 }
 
 void Renderer::renderPolygon(const std::vector<Point>& vertices) {};
@@ -122,7 +149,7 @@ void Renderer::drawGrid()
 void Renderer::drawVerticalLine(int x, int y1, int y2)
 {
   if (y1 > y2) std::swap(y1, y2);
-  for (int y = y1; y1 < y2; y++)
+  for (int y = y1; y < y2; y++)
   {
     renderPoint(x, y);
   };
@@ -131,7 +158,7 @@ void Renderer::drawVerticalLine(int x, int y1, int y2)
 void Renderer::drawHorizontalLine(int x1, int x2, int y)
 {
   if (x1 > x2) std::swap(x1, x2);
-  for (int x = x1; x1 < x2; x++)
+  for (int x = x1; x < x2; x++)
   {
     renderPoint(x, y);
   }
@@ -152,24 +179,46 @@ void Renderer::drawDiagonalLine(int x1, int y1, int x2, int y2)
 };
 
 void Renderer::drawLineBresenham(int x1, int y1, int x2, int y2) {
-  // int dx = abs(x2 - x1);
-  // int dy = abs(y2 - y1);
-  // float m = static_cast<float>(dx) / static_cast<float>(dy);
-  // int x, y = x1, y1;
-  // float error = 0.5f;
+  int dx = abs(x2 - x1);
+  int dy = abs(y2 - y1);
+  float error = 0.5f;
+  int x = x1;
+  int y = y1;
+  int stepX = dx > 0 ? 1 : -1;
+  int stepY = dy > 0 ? 1 : -1;
 
-  // while (x < x2)
-  // {
-  //   renderPoint(x,y);
-  //   x++;
-  //   error += m; // how much change in y for each one change in x
-  //   if (error >= 0.5f) // 0.5 represents a half-pixel here?
-  //   {
-  //     y++;
-  //     error -= 1;
-  //   }
-  // }
-  // renderPoint(x,y);
+
+  if (dx > dy)
+  {
+
+    float m = static_cast<float>(dx) / static_cast<float>(dy);
+    while (x < x2)
+    {
+      renderPoint(x,y);
+      x += stepX;
+      error += m; // how much change in y for each one change in x
+      if (error >= 0.5f) // 0.5 represents a half-pixel here?
+      {
+        y += stepY;
+        error -= 1;
+      }
+    }
+    renderPoint(x,y);
+  } else {
+    float m = static_cast<float>(dy) / static_cast<float>(dx);
+    while (y < y2)
+    {
+      renderPoint(x,y);
+      y += stepY;
+      error += m; // how much change in y for each one change in x
+      if (error >= 0.5f) // 0.5 represents a half-pixel here?
+      {
+        x += stepX;
+        error -= 1;
+      }
+    }
+  }
+  renderPoint(x,y);
 };
 
 
